d217beb2d9943a187ccf8db3187438433b359723
BerpL <eder.cornejo.d@gmail.com>
2021-10-04 12:29:59 -0500

    web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ConocenosComponent } from './pages/conocenos/conocenos.component';
+import { CampaniaComponent } from './pages/campania/campania.component';
+import { ReclamosComponent } from './pages/reclamos/reclamos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { MisionyvisionComponent } from './pages/misionyvision/misionyvision.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ConocenosComponent,
+    CampaniaComponent,
+    ReclamosComponent,
+    ContactoComponent,
+    MisionyvisionComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/misionyvision/misionyvision.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-misionyvision',
+  templateUrl: './misionyvision.component.html',
+  styles: [
+  ]
+})
+export class MisionyvisionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
