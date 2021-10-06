fd9c0dfdc1aff3928e0305ee5ce3f3ec9d3e8e70
BerpL <eder.cornejo.d@gmail.com>
2021-10-06 01:38:55 -0500

    final

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -12,6 +12,7 @@ export class NavbarComponent implements OnInit {
   flagcampania: boolean = false;
   flagreclamos: boolean = false;
   flagcontacto: boolean = false;
+  flagmisionyvision:boolean = false;
 
   constructor() { }
 
@@ -25,26 +26,38 @@ export class NavbarComponent implements OnInit {
       this.flagcampania = false;
       this.flagreclamos = false;
       this.flagcontacto = false;
+      this.flagmisionyvision = false;
     } else if(id == "conocenos"){
       this.flagconocenos = true;
       this.flaginicio = false;
       this.flagcampania = false;
       this.flagreclamos = false;
       this.flagcontacto = false;
+      this.flagmisionyvision = false;
     } else if(id == "campania"){
       this.flagcampania = true;
       this.flaginicio = false;
       this.flagconocenos = false;
       this.flagreclamos = false;
       this.flagcontacto = false;
+      this.flagmisionyvision = false;
     } else if(id == "reclamos"){
       this.flagreclamos = true;
       this.flaginicio = false;
       this.flagconocenos = false;
       this.flagcampania = false;
       this.flagcontacto = false;
+      this.flagmisionyvision = false;
     } else if(id == "contacto"){
       this.flagcontacto = true;
+      this.flaginicio = false;
+      this.flagconocenos = false;
+      this.flagcampania = false;
+      this.flagreclamos = false;
+      this.flagmisionyvision = false;
+    } else if(id == "misionyvision"){
+      this.flagmisionyvision = true;
+      this.flagcontacto = false;
       this.flaginicio = false;
       this.flagconocenos = false;
       this.flagcampania = false;
